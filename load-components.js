c96f03c7010f04b905b5e4b9c4088ef592687d4c
kyleex <antwill34@icloud.com>
2023-12-28 04:05:15 +0100

    add navigation menu in header

--- load-components.js
+++ load-components.js
@@ -1,15 +1,36 @@
-// Cette fonction charge le contenu de l'en-tête et du pied de page et les insère dans les conteneurs
-function loadComponent(componentPath, containerId) {
-  fetch(componentPath)
+// Fonction pour charger un composant dans un conteneur spécifié par son ID
+function loadComponent(url, containerId, callback) {
+  fetch(url)
     .then((response) => response.text())
-    .then((data) => {
-      document.getElementById(containerId).innerHTML = data;
+    .then((html) => {
+      document.getElementById(containerId).innerHTML = html;
+      if (typeof callback === "function") {
+        callback();
+      }
     })
-    .catch((err) => console.error(`Could not load ${componentPath}:`, err));
+    .catch((err) => console.error(`Error loading ${url}: `, err));
 }
 
-// Utilisation de la fonction pour charger l'en-tête et le pied de page
-document.addEventListener("DOMContentLoaded", () => {
-  loadComponent("components/header.html", "header-container");
-  // loadComponent("footer.html", "footer-container");
+// Chargez le contenu du header et du nav à l'aide de la fonction définie ci-dessus
+
+// Exemple d'utilisation pour charger le header
+loadComponent("components/header.html", "header-container", function () {
+  // Assurez-vous que l'icône hamburger est présente puis créez l'écouteur d'événement
+  let openMenuBtn = document.getElementById("open-menu-btn");
+  if (openMenuBtn) {
+    openMenuBtn.addEventListener("click", openMenu);
+  } else {
+    console.error("Hamburger icon not found after loading header");
+  }
+});
+
+// Charger la barre de navigation
+loadComponent("components/nav.html", "nav-container", function () {
+  // Assurez-vous que le bouton de fermeture du menu est là, puis mettez en place l'écouteur d'événements
+  let closeMenuBtn = document.getElementById("close-menu-btn");
+  if (closeMenuBtn) {
+    closeMenuBtn.addEventListener("click", closeMenu);
+  } else {
+    console.error("Close menu button not found after loading navbar");
+  }
 });
---
+++ script.js
@@ -0,0 +1,49 @@
+// S'exécute lorsque le DOM est entièrement chargé
+document.addEventListener("DOMContentLoaded", function () {
+  // Récupération de l'élément open-menu-btn et close-menu-btn
+  let openMenuBtn = document.getElementById("open-menu-btn");
+  let closeMenuBtn = document.getElementById("close-menu-btn");
+
+  // Vérification de l'existence de l'élément openMenuBtn avant de continuer
+  if (openMenuBtn) {
+    openMenuBtn.addEventListener("click", openMenu);
+  } else {
+    // Si l'élément n'existe pas, affichage d'un message d'erreur dans la console
+    console.error("Open menu button not found");
+  }
+
+  // Vérification de l'existence de l'élément closeMenuBtn avant de continuer
+  if (closeMenuBtn) {
+    closeMenuBtn.addEventListener("click", closeMenu);
+  } else {
+    // Si l'élément n'existe pas, affichage d'un message d'erreur dans la console
+    console.error("Close menu button not found");
+  }
+});
+
+// Fonction pour ouvrir le menu
+function openMenu() {
+  let sidebar = document.getElementById("sidebar");
+  if (sidebar) {
+    sidebar.style.display = "flex"; // ou 'block' si votre sidebar n'est pas un flex container
+  } else {
+    console.error("The sidebar element was not found.");
+  }
+}
+
+// Fonction pour fermer le menu
+function closeMenu() {
+  let sidebar = document.getElementById("sidebar"); // Trouve l'élément sidebar par son ID
+  if (sidebar) {
+    sidebar.style.display = "none"; // Change le style `display` pour cacher la sidebar
+  } else {
+    console.error("The sidebar element was not found.");
+  }
+}
+
+// Attacher l'événement de clic à l'icône hamburger pour basculer le menu
+
+// Ce code initie l'écouteur d'événement au clic sur l'icône hamburger. Lorsque l'icône est cliquée,
+// la fonction `toggleMenu` est appelée. La fonction `toggleMenu` vérifie l'état de la sidebar,
+// et en fonction de cet état, soit appelle `openMenu` pour afficher la sidebar,
+// soit appelle `closeMenu` pour cacher la sidebar.
